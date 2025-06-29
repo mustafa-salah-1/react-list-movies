f8359f68c024c0a4099f57a7ab9cd71222e98a5d
mustafa <mustafa.salah.2131@gmail.com>
2025-06-29 12:34:13 +0300

    Implements basic movie list layout
    Implements a basic layout for displaying a list of movies, including a navigation bar with search functionality.
    
    The layout includes placeholders for movie data and styling to provide a rudimentary user interface.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,40 @@
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <nav>
+        <div>
+          <img src="logo192.png" width={35} alt="logo" />
+          <span>Movies</span>
+        </div>
+        <div>
+          <input type="text" placeholder="Search..." />
+        </div>
+        <div>
+          <p>
+            Result <strong>X</strong>
+          </p>
+        </div>
+      </nav>
+      <main>
+        <div className="movie">
+          <div className="list">
+            <div className="item"></div>
+          </div>
+        </div>
+        <div className="movie">
+          <div className="list">
+            <div className="item">
+              <div>
+                <img src="logo512.png" width={55} alt="test" />
+              </div>
+              <div>
+                <h2>name</h2>
+                <p>des</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
