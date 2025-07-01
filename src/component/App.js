ce15fcafb54a141a330dc4d4714d26f073bc3410
mustafa <mustafa.salah.2131@gmail.com>
2025-07-01 22:25:14 +0300

    Refactors App component
    Refactors the App component by extracting the ShowMovie component to its own file for better organization.
    
    This improves code readability and maintainability by separating concerns. Also adds Loader component.

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import StarRating from "./StarRating";
+import { useEffect, useState } from "react"; 
+import ShowMovie from "./ShowMovie";
+import Loader from "./Loader";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
@@ -103,137 +104,6 @@ export default function App() {
 
 function Main({ children }) {
   return <main>{children}</main>;
-}
-
-function ShowMovie({ movieID, handleWatchedMoive, isRate }) {
-  const [movie, setMovie] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [star, setStar] = useState();
-
-  function handleAddMovieToList() {
-    handleWatchedMoive({
-      id: movie.imdbID,
-      title: movie.Title,
-      poster: movie.Poster,
-      time: movie.Runtime.replace(" min", ""),
-      year: movie.Year,
-      rate: movie.imdbRating,
-      myRate: star,
-    });
-  }
-
-  function handleClickStar(star) {
-    setStar(() => star);
-  }
-
-  useEffect(
-    function () {
-      async function getMovieById() {
-        try {
-          setIsLoading(true);
-          const result = await fetch(
-            `https://www.omdbapi.com/?apikey=4b839182&i=${movieID}`
-          );
-
-          if (!result.ok) throw new Error("fetch movies falied");
-
-          const data = await result.json();
-
-          if (data.Response === "False") throw new Error("movie not found!");
-
-          setMovie(data);
-        } catch (err) {
-          console.log(err.message);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      setStar(null);
-      getMovieById();
-    },
-    [movieID]
-  );
-
-  return (
-    <div>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <div style={{ display: "flex", gap: "5px" }}>
-            <img
-              src={movie.Poster}
-              style={{ borderRadius: "10px", width: "170px" }}
-              alt={movie.Title}
-            />
-            <div>
-              <h3>{movie.Title}</h3>
-              <p>
-                {movie.Released} | {movie.Runtime}
-              </p>
-              <p>{movie.Genre}</p>
-              <p>⭐ {movie.imdbRating}</p>
-            </div>
-          </div>
-          <div style={{ marginBlock: "20px" }}>
-            {!isRate ? (
-              <>
-                <StarRating
-                  defaultRate={movie.imdbRating}
-                  starLength={10}
-                  onClick={handleClickStar}
-                />
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                  }}
-                >
-                  {star && (
-                    <button
-                      onClick={handleAddMovieToList}
-                      style={{
-                        fontSize: "14px",
-                        backgroundColor: "yellow",
-                        color: "black",
-                        width: "auto",
-                        padding: "15px",
-                        borderRadius: "10px",
-                      }}
-                    >
-                      + add to watched list
-                    </button>
-                  )}
-                </div>
-              </>
-            ) : (
-              <h3
-                style={{
-                  backgroundColor: "#1a1a1d",
-                  paddingBlock: "5px",
-                  borderRadius: "10px",
-                  textAlign: "center",
-                  fontWeight: "600",
-                }}
-              >
-                You rated this movie
-              </h3>
-            )}
-          </div>
-          <div
-            style={{
-              backgroundColor: "#1a1a1d",
-              padding: "10px",
-              borderRadius: "10px",
-            }}
-          >
-            {movie.Plot}
-          </div>
-        </>
-      )}
-    </div>
-  );
 }
 
 function Box({ children, movie = null, handleShowMovie = null }) {
@@ -343,13 +213,6 @@ function WatchedItem({ movie, handleRemove }) {
   );
 }
 
-function Loader() {
-  return (
-    <div style={{ padding: "5px", textAlign: "center", fontSize: "22px" }}>
-      Loading...
-    </div>
-  );
-}
 function ErrorMessage({ error }) {
   return (
     <div style={{ padding: "5px", textAlign: "center", fontSize: "22px" }}>
@@ -365,6 +228,7 @@ function Navbar({ children }) {
     </nav>
   );
 }
+
 function Logo() {
   return (
     <div>
---
+++ src/component/Loader.js
@@ -0,0 +1,8 @@
+
+export default function Loader() {
+  return (
+    <div style={{ padding: "5px", textAlign: "center", fontSize: "22px" }}>
+      Loading...
+    </div>
+  );
+}
